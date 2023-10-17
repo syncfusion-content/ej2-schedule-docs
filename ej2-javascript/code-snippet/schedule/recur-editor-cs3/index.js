a440ffa6d8b1d1c258e08a3306d82531f083ea30
Satheesh Kumar Balasubramanian <satheeshkumar.balasubramanian@syncfusion.com>
2023-10-17 17:18:01 +0530

    documentation(ES-852376): Cleared the preview sample issues for our schedule samples in typescript and javascript platform

--- ej2-javascript/code-snippet/schedule/recur-editor-cs3/index.js
+++ ej2-javascript/code-snippet/schedule/recur-editor-cs3/index.js
@@ -4,17 +4,31 @@ var recObject = new ej.schedule.RecurrenceEditor({
   change: OnRecurrenceChange,
 });
 recObject.appendTo('#RecurrenceEditor');
+var ruleString = 'FREQ=DAILY;INTERVAL=1';
+recObject.setRecurrenceRule(ruleString);
 labelElement.innerText = 'Date Collections';
 outputElement.innerHTML = '';
+var ruleStringDates = recObject.getRecurrenceDates(new Date(), ruleString);
+for (var index = 0; index < ruleStringDates.length; index++) {
+  outputElement.appendChild(
+    new ej.base.createElement('div', {
+      innerHTML: new Date(ruleStringDates[index]).toString(),
+    })
+  );
+}
 
 function OnRecurrenceChange(args) {
-  outputElement.innerHTML = '';
-  let dates = recObject.getRecurrenceDates(new Date(), args.value);
-  for (var index = 0; index < dates.length; index++) {
-    outputElement.appendChild(
-      new ej.base.createElement('div', {
-        innerHTML: new Date(dates[index]).toString(),
-      })
-    );
+  if (args.value == '') {
+    outputElement.innerText = 'Select Rule';
+  } else {
+    outputElement.innerHTML = '';
+    let dates = recObject.getRecurrenceDates(new Date(), args.value);
+    for (var index = 0; index < dates.length; index++) {
+      outputElement.appendChild(
+        new ej.base.createElement('div', {
+          innerHTML: new Date(dates[index]).toString(),
+        })
+      );
+    }
   }
 }
--- ej2-javascript/code-snippet/schedule/recur-editor-cs3/index.ts
+++ ej2-javascript/code-snippet/schedule/recur-editor-cs3/index.ts
@@ -7,15 +7,32 @@ let recObject: RecurrenceEditor = new RecurrenceEditor({
   change: OnRecurrenceChange,
 });
 recObject.appendTo('#RecurrenceEditor');
+let ruleString: string = 'FREQ=DAILY;INTERVAL=1';
+recObject.setRecurrenceRule(ruleString);
 labelElement.innerText = 'Date Collections';
 outputElement.innerHTML = '';
+let ruleStringDates: number[] = recObject.getRecurrenceDates(
+  new Date(),
+  ruleString
+);
+for (let index: number = 0; index < ruleStringDates.length; index++) {
+  outputElement.appendChild(
+    createElement('div', {
+      innerHTML: new Date(ruleStringDates[index]).toString(),
+    })
+  );
+}
 
 function OnRecurrenceChange(args: RecurrenceEditorChangeEventArgs): void {
-  outputElement.innerHTML = '';
-  let dates: number[] = recObject.getRecurrenceDates(new Date(), args.value);
-  for (let index: number = 0; index < dates.length; index++) {
-    outputElement.appendChild(
-      createElement('div', { innerHTML: new Date(dates[index]).toString() })
-    );
+  if (args.value == '') {
+    outputElement.innerText = 'Select Rule';
+  } else {
+    outputElement.innerHTML = '';
+    let dates: number[] = recObject.getRecurrenceDates(new Date(), args.value);
+    for (let index: number = 0; index < dates.length; index++) {
+      outputElement.appendChild(
+        createElement('div', { innerHTML: new Date(dates[index]).toString() })
+      );
+    }
   }
 }
