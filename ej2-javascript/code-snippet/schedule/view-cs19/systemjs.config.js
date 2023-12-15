224e5d25865b966bf9d82c022a61d6e7837ad0c5
vijayravi161099 <vijay.ravi@syncfusion.com>
2023-12-15 15:05:09 +0530

    documentation(ES-859784-MaxEventsUG): Added maxEventsPerRow property in schedule docs.

--- ej2-javascript/code-snippet/schedule/view-cs19/systemjs.config.js
+++ ej2-javascript/code-snippet/schedule/view-cs19/systemjs.config.js
@@ -10,7 +10,7 @@ System.config({
         }
     },
     paths: {
-        "syncfusion:": "https://cdn.syncfusion.com/ej2/24.1.1/"
+        "syncfusion:": "https://cdn.syncfusion.com/ej2/20.3.56/"
     },
     map: {
         main: "index.ts",
