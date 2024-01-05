1dbfaec6cf52ea48206de020327de6969ecd5478
vijayravi161099 <vijay.ravi@syncfusion.com>
2024-01-05 18:25:45 +0530

    documentation(ES-864834-SampleCorrections): Correction in New added feature sample.

--- ej2-javascript/code-snippet/schedule/view-cs19/index.js
+++ ej2-javascript/code-snippet/schedule/view-cs19/index.js
@@ -1,11 +1,8 @@
 var scheduleObj = new ej.schedule.Schedule({
     width: '100%',
-    height: '550px',
+    height: '380px',
     selectedDate: new Date(2023, 11, 15),
-    views: [{ option: 'Month', maxEventsPerRow: 3 },
-            { option: 'TimelineMonth', maxEventsPerRow: 3 },
-            { option: 'TimelineYear', displayName: 'Horizontal Timeline Year', maxEventsPerRow: 3 },
-            { option: 'TimelineYear', displayName: 'Vertical Timeline Year', orientation: 'Vertical', maxEventsPerRow: 3 }],
+    views: [{ option: 'Month', maxEventsPerRow: 3 }],
     eventSettings: { dataSource: scheduleData }
 });
 scheduleObj.appendTo('#Schedule');
--- ej2-javascript/code-snippet/schedule/view-cs19/index.ts
+++ ej2-javascript/code-snippet/schedule/view-cs19/index.ts
@@ -6,12 +6,9 @@ import { scheduleData } from './datasource.ts';
 Schedule.Inject(Month, Year, TimelineYear, TimelineMonth);
 let scheduleObj: Schedule = new Schedule({
     width: '100%',
-    height: '550px',
+    height: '380px',
     selectedDate: new Date(2023, 11, 15),
-    views: [{ option: 'Month', maxEventsPerRow: 3 },
-            { option: 'TimelineMonth', maxEventsPerRow: 3 },
-            { option: 'TimelineYear', displayName: 'Horizontal Timeline Year', maxEventsPerRow: 3 },
-            { option: 'TimelineYear', displayName: 'Vertical Timeline Year', orientation: 'Vertical', maxEventsPerRow: 3 }],
+    views: [{ option: 'Month', maxEventsPerRow: 3 }],
     eventSettings: { dataSource: scheduleData }
 });
 scheduleObj.appendTo('#Schedule');
